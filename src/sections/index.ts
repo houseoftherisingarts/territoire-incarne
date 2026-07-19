eb5c911b15ee97eed340db80f0a0079d3f88db4d
alextstlaurent <alex@lesalondesinconnus.com>
2026-07-19 12:16:31 -0400

    autosave: 2026-07-19 12:16:31

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,7 +1,6 @@
 export { Apropos } from "./Apropos";
 export { Boutique } from "./Boutique";
 export { Connecter } from "./Connecter";
-export { Education } from "./Education";
 export { Events } from "./Events";
 export { Mouvement, MouvementSidebarForm } from "./Mouvement";
 export { Ressources } from "./Ressources";
